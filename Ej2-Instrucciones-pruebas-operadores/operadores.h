58ab83a16c6e5b5a911cb56241e6f49eabc27ded
Morphos-afk <jbalbsan@myuax.com>
2024-11-02 17:53:17 +0100

    2 comit

---
+++ Ej2-Instrucciones-pruebas-operadores/operadores.h
@@ -0,0 +1,12 @@
+// operadores.h
+#include <locale>
+#include <codecvt>
+
+#ifndef OPERADORES_H
+#define OPERADORES_H
+
+void demostrarOperadoresAritmeticos();
+void demostrarOperadoresComparacion();
+void demostrarOperadoresLogicos();
+
+#endif // OPERADORES_H
